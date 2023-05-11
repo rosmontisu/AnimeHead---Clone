aa35aae5776b4e156d3b73fd204a26a74b53c09c
nyuuzyou <httsmvkcom@duck.com>
2023-05-11 22:20:06 +0300

    added dataset without augmentation, changed the script

--- AnimeHeadsv3.py
+++ AnimeHeadsv3.py
@@ -1,63 +1,51 @@
-import os
+import collections
 import json
+import os
 import datasets
 
+ANNOTATION_FILENAME = "_annotations.coco.json"
 
-class AnimeHeadsv3(datasets.GeneratorBasedBuilder):
+class AHv3Config(datasets.BuilderConfig):
+    def __init__(self, data_urls, **kwargs):
+        super(AHv3Config, self).__init__(**kwargs)
+        self.data_urls = data_urls
+
+class AHv3(datasets.GeneratorBasedBuilder):
+    BUILDER_CONFIGS = [
+        AHv3Config(name="With augmentation", data_urls={"train": "AHv3-AUG/train.zip", "validation": "AHv3-AUG/valid.zip", "test": "AHv3-AUG/test.zip"}),
+        AHv3Config(name="Without augmentation", data_urls={"train": "AHv3-NA/train.zip", "validation": "AHv3-NA/valid.zip", "test": "AHv3-NA/test.zip"}),
+    ]
+
     def _info(self):
-        return datasets.DatasetInfo(
-            features=datasets.Features(
-                {
-                    "image": datasets.Image(),
-                }
-            )
-        )
+        features = datasets.Features({"image_id": datasets.Value("int64"), "image": datasets.Image(), "width": datasets.Value("int32"), "height": datasets.Value("int32"), "objects": datasets.Sequence({"id": datasets.Value("int64"), "area": datasets.Value("int64"), "bbox": datasets.Sequence(datasets.Value("float32"), length=4), "category": datasets.Value("string")})})
+        return datasets.DatasetInfo(features=features)
 
     def _split_generators(self, dl_manager):
-        _URLS = {
-            "images": "images.zip",
-            "train_data": "annotations_train.json",
-            "test_data": "annotations_test.json",
-            "valid_data": "annotations_valid.json"
-        }
-        data_files = dl_manager.download_and_extract(_URLS)
+        data_files = dl_manager.download_and_extract(self.config.data_urls)
+        return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"folder_dir": data_files["train"]}), datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"folder_dir": data_files["validation"]}), datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"folder_dir": data_files["test"]})]
 
-        return [
-            datasets.SplitGenerator(
-                name=datasets.Split.TRAIN,
-                gen_kwargs={
-                    "image_paths": dl_manager.iter_files(data_files["images"]),
-                    "annotation_path": data_files["train_data"],
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TEST,
-                gen_kwargs={
-                    "image_paths": dl_manager.iter_files(data_files["images"]),
-                    "annotation_path": data_files["test_data"],
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                gen_kwargs={
-                    "image_paths": dl_manager.iter_files(data_files["images"]),
-                    "annotation_path": data_files["valid_data"],
-                },
-            )
-        ]
+    def _generate_examples(self, folder_dir):
+        def process_annot(annot, category_id_to_category):
+            return {"id": annot["id"], "area": annot["area"], "bbox": annot["bbox"], "category": category_id_to_category[annot["category_id"]]}
 
-    def _generate_examples(self, image_paths, annotation_path):
-        """Generate examples."""
-        with open(annotation_path, 'r') as f:
-            data = json.load(f)
+        image_id_to_image = {}
+        idx = 0
 
-        image_names = set()
-        for image_data in data['images']:
-            image_names.add(image_data['file_name'])
+        annotation_filepath = os.path.join(folder_dir, ANNOTATION_FILENAME)
+        with open(annotation_filepath, "r") as f:
+            annotations = json.load(f)
+        category_id_to_category = {category["id"]: category["name"] for category in annotations["categories"]}
+        image_id_to_annotations = collections.defaultdict(list)
+        for annot in annotations["annotations"]:
+            image_id_to_annotations[annot["image_id"]].append(annot)
+        filename_to_image = {image["file_name"]: image for image in annotations["images"]}
 
-        for idx, image_path in enumerate(image_paths):
-            if os.path.basename(image_path) in image_names:
-                example = {
-                    "image": image_path,
-                }
-                yield idx, example
+        for filename in os.listdir(folder_dir):
+            filepath = os.path.join(folder_dir, filename)
+            if filename in filename_to_image:
+                image = filename_to_image[filename]
+                objects = [process_annot(annot, category_id_to_category) for annot in image_id_to_annotations[image["id"]]]
+                with open(filepath, "rb") as f:
+                    image_bytes = f.read()
+                yield idx, {"image_id": image["id"], "image": {"path": filepath, "bytes": image_bytes}, "width": image["width"], "height": image["height"], "objects": objects}
+                idx += 1
--- README.md
+++ README.md
@@ -2,18 +2,99 @@
 task_categories:
 - object-detection
 license: wtfpl
+dataset_info:
+- config_name: With augmentation
+  features:
+  - name: image_id
+    dtype: int64
+  - name: image
+    dtype: image
+  - name: width
+    dtype: int32
+  - name: height
+    dtype: int32
+  - name: objects
+    sequence:
+    - name: id
+      dtype: int64
+    - name: area
+      dtype: int64
+    - name: bbox
+      sequence: float32
+      length: 4
+    - name: category
+      dtype: string
+  splits:
+  - name: train
+    num_bytes: 2817954
+    num_examples: 8037
+  - name: validation
+    num_bytes: 37647
+    num_examples: 100
+  - name: test
+    num_bytes: 8425
+    num_examples: 20
+  download_size: 590150250
+  dataset_size: 2864026
+- config_name: Without augmentation
+  features:
+  - name: image_id
+    dtype: int64
+  - name: image
+    dtype: image
+  - name: width
+    dtype: int32
+  - name: height
+    dtype: int32
+  - name: objects
+    sequence:
+    - name: id
+      dtype: int64
+    - name: area
+      dtype: int64
+    - name: bbox
+      sequence: float32
+      length: 4
+    - name: category
+      dtype: string
+  splits:
+  - name: train
+    num_bytes: 932413
+    num_examples: 2659
+  - name: validation
+    num_bytes: 37647
+    num_examples: 100
+  - name: test
+    num_bytes: 7393
+    num_examples: 18
+  download_size: 512953012
+  dataset_size: 977453
 ---
+
 # AnimeHeadsv3 Object Detection Dataset
 
-The AnimeHeadsv3 Object Detection Dataset is a collection of anime and art images, including manga pages, that have been annotated with object bounding boxes for use in object detection tasks. This dataset was used to train the final version of the [Anime Object Detection Models](https://huggingface.co/nyuuzyou/AnimeHeads).
-
+The AnimeHeadsv3 Object Detection Dataset is a collection of anime and art images, including manga pages, that have been annotated with object bounding boxes for use in object detection tasks.
 ## Contents
 
+There are two versions of the dataset available:
 The dataset contains a total of 8157 images, split into training, validation, and testing sets. The images were collected from various sources and include a variety of anime and art styles, including manga.
 
-The annotations were created using the COCO format, with each annotation file containing the bounding box coordinates and label for each object in the corresponding image. The dataset has only one class named "head".
+- Dataset with augmentation: Contains 8157 images.
+- Dataset without augmentation: Contains 2777 images.
 
-Augmentation parameters:
+The images were collected from various sources and include a variety of anime and art styles, including manga. The annotations were created using the COCO format, with each annotation file containing the bounding box coordinates and label for each object in the corresponding image. The dataset has only one class named "head".
+
+## Preprocessing
+
+The dataset with augmentation has the following preprocessing parameters:
+
+    Resize: Fit within 640x640
+
+The dataset without augmentation does not have any preprocessing applied.
+
+## Augmentation Parameters
+
+The following augmentation parameters were applied to the dataset with augmentation:
 
     Outputs per training example: 3
     Flip: Horizontal
